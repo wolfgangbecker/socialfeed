462c5d49c7454a5a67654a112be238eb8b78c77c
Wolfgang <wolli.becker@gmail.com>
2014-09-21 17:48:43 -0300

    setup devise routes, convert views erb to haml and generate home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Socialfeed::Application.routes.draw do
+  get "home/index"
+
   devise_for :users
 
+  devise_scope :user do
+    authenticated :user do
+      root to: 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +61,6 @@ Socialfeed::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
