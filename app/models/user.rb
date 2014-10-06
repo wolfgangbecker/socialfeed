6d7e9e75f0c93a07c2b868923ffccd2e6014c8bf
donboli <wolli.becker@gmail.com>
2014-10-06 20:52:42 -0300

    adapt devise to hobofields

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,21 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  fields do
+  	name 					:string	
+  	email					:string, default: "", null: false, index: "index_users_on_email", unique: true
+  	encrypted_password		:string, default: "", null: false
+  	reset_password_token	:string, index: "index_users_on_reset_password_token", unique: true
+  	reset_password_sent_at	:datetime
+  	remember_created_at		:datetime
+  	sign_in_count			:integer, default: 0, null: false
+  	current_sign_in_at		:datetime
+  	last_sign_in_at			:datetime
+  	current_sign_in_ip		:string
+  	last_sign_in_ip			:string
+  	remember_created_at 	:datetime
+  	created_at 				:datetime, null: false
+  	updated_at 				:datetime, null: false
+  end
+
 end
