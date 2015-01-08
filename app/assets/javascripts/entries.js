e4cd3bd47ca8fb5bb9f07f0d7ab43bf61ec608d4
Juan Becker <wolli.becker@gmail.com>
2015-01-08 16:25:05 -0300

    add ajax to entries/index read_link

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -1,2 +1,25 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+(function(){
+
+	$(document).ready(function(){
+		$('.js-entries')
+		.on('ajax:success', '.js-entry', function(e, data){
+			if(data.read){
+				$(this).addClass('read');
+				$(this).find('.js-read-link').text('Read');
+				$(this).find('.js-read-link').attr('href', $(this).find('.js-read-link').attr('href').replace('read%5D=true', 'read%5D=false'));
+			}
+			else{
+				$(this).removeClass('read');
+				$(this).find('.js-read-link').text('Unread');
+				$(this).find('.js-read-link').attr('href', $(this).find('.js-read-link').attr('href').replace('read%5D=false', 'read%5D=true'));
+			}
+			data.favorite ? $(this).find('.icon').addClass('read') : $(this).find('.icon').removeClass('read');
+		})
+		.on('ajax:error', '.js-entry', function(e, data){
+		});
+	});
+
+})();
--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,9 +1,15 @@
 module EntriesHelper
 	def read_link entry
-		link_to entry.read, entry_path(entry, params: {entry: {read: !entry.object.read}}), method: :put
+		link_to entry.read, entry_path(entry, params: {entry: {read: !entry.object.read}}), method: :put, :remote => true, data: {type: :json}, class: 'js-read-link'
+	end
+
+	def favorite_link entry
+		link_to entry_path(entry, params: {entry: {favorite: !entry.object.favorite}}), method: :put do
+			content_tag(:div, 'c', class: "icon pull-right #{'favorited' if entry.favorite?}")
+		end
 	end
 
 	def read? entry
-		entry.read?? 'read' : 'unread'
+		'read' if entry.read?
 	end
 end
