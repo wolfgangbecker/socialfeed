f04162b12fcd7904b27f1859bad5aa251df90a05
Juan Becker <wolli.becker@gmail.com>
2015-01-11 15:15:39 -0300

    schedule deletion for old feed entries

---
+++ app/workers/entry_worker.rb
@@ -0,0 +1,15 @@
+class EntryWorker
+  include Sidekiq::Worker
+  include Sidetiq::Schedulable
+
+  recurrence { hourly.minute_of_hour(1, 16, 31, 46) }
+
+  def perform
+    feed_ids = Feed.pluck('DISTINCT id')
+    feed_ids.each do |feed_id|
+			cutoff = Entry.where(feed_id: feed_id).order('published_at DESC').offset(50).first
+			Entry.where(feed_id: feed_id).where(['created_at <= ?', cutoff.published_at]).delete_all
+		end
+  rescue Exception => e
+  end
+end
