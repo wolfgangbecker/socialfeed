942c54072529793f6eb8019f1c96d732d1d3d68b
Juan Becker <wolli.becker@gmail.com>
2015-01-12 13:14:19 -0300

    change entries deletion time schedule and correct date parameter

--- app/workers/entry_worker.rb
+++ app/workers/entry_worker.rb
@@ -2,13 +2,13 @@ class EntryWorker
   include Sidekiq::Worker
   include Sidetiq::Schedulable
 
-  recurrence { hourly.minute_of_hour(1, 16, 31, 46) }
+  recurrence { daily.hour_of_day(0).minute_of_hour(1) }
 
   def perform
     feed_ids = Feed.pluck('DISTINCT id')
     feed_ids.each do |feed_id|
 			cutoff = Entry.where(feed_id: feed_id).order('published_at DESC').offset(50).first
-			Entry.where(feed_id: feed_id).deletable.where(['created_at <= ?', cutoff.published_at]).delete_all
+			Entry.where(feed_id: feed_id).deletable.where(['published_at <= ?', cutoff.published_at]).delete_all
 		end
   rescue Exception => e
   end
