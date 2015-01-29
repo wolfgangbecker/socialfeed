4e2941df70763e42d100e1ad9519be38bf8d65d6
Juan Becker <wolli.becker@gmail.com>
2015-01-28 21:16:01 -0300

    highlight selected sidebar items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def sidebar_link_to name, controller_name
+    link_to send("#{name}_path"), class: "text-center #{'selected' if controller_name == name}" do
+      I18n.t("layouts.aside.#{name}")
+    end
+  end
 end
