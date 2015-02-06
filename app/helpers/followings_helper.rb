40f604ccc72d7b52c368d299dadb1263c18be49a
Juan Becker <wolli.becker@gmail.com>
2015-02-06 17:05:51 -0300

    add AJAX to followings create action response

--- app/helpers/followings_helper.rb
+++ app/helpers/followings_helper.rb
@@ -1,2 +1,13 @@
 module FollowingsHelper
+  def toggle_follow_button follower, followed
+    if follower.followed_ids.include? followed.id
+      link_to following_path(id: follower.followings.select {|f| f.followed_id == followed.id}.first.id), method: :delete, remote: true do
+        content_tag(:div, 'e', class: "btn icon")
+      end
+    else
+      link_to followings_path(followed_id: followed.id), method: :post, remote: true do
+        content_tag(:div, 'g', class: "btn icon")
+      end
+    end
+  end
 end
